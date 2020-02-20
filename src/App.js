d34836198abb765d17770d2c47b1e175ad3f4028
YuliiaBrakova <yuliia.brakova@gmail.com>
2020-02-20 19:24:32 +0200

    для записи элементов с новой строки после return  пишем ()

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import React from 'react'
 // }
 
 const App = () => {
-		return <div>
-		<h1>Hello</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, porro doloribus. Aliquam at natus inventore illum incidunt. Dolorem deleniti doloribus sequi culpa corrupti? Aliquam error ullam molestias dolorem vel earum?</p>
-		<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque assumenda, repellat odio libero molestiae odit obcaecati, blanditiis temporibus, esse pariatur eligendi. Minima hic ratione unde maxime quae in esse nam!</p>
+		return (
+		<div>
+			<h1>Hello</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, porro doloribus. Aliquam at natus inventore illum incidunt. Dolorem deleniti doloribus sequi culpa corrupti? Aliquam error ullam molestias dolorem vel earum?</p>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque assumenda, repellat odio libero molestiae odit obcaecati, blanditiis temporibus, esse pariatur eligendi. Minima hic ratione unde maxime quae in esse nam!</p>
 		</div>
+		)
 }
 export default App
