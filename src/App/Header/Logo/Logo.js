101078e633888207d2c46bdeb0a0990cb3a2cad4
YuliiaBrakova <yuliia.brakova@gmail.com>
2020-02-20 20:54:43 +0200

    розбили на компоненты logo,menu,cart

---
+++ src/App/Header/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+            <div className="logo">
+                <img src="img/logo.png" alt="" />
+            </div>
+    )
+}
+export default Logo 
