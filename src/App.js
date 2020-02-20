fbf7728113ba19a82c05c8299d19f65e17180508
YuliiaBrakova <yuliia.brakova@gmail.com>
2020-02-20 19:35:01 +0200

    компоненты нужно писать с большой буквы, так как браузер будет выводить как тег

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
-// function App() {
-// 	return <h1>Hello world</h1>
-// }
 
 const App = () => {
 		return (
 		<div>
-			<h1>Hello</h1>
+			<Header />
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, porro doloribus. Aliquam at natus inventore illum incidunt. Dolorem deleniti doloribus sequi culpa corrupti? Aliquam error ullam molestias dolorem vel earum?</p>
 			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque assumenda, repellat odio libero molestiae odit obcaecati, blanditiis temporibus, esse pariatur eligendi. Minima hic ratione unde maxime quae in esse nam!</p>
 		</div>
 		)
+}
+
+const Header = () => {
+	return (
+		<h1>Hello1</h1>
+	)
 }
 export default App
