d0db266f7834265daeeb2ef91d9d7502ecc1d2d0
Yuliia <yuliia.brakova@gmail.com>
2020-02-25 19:21:37 +0200

    добавили стили через файл base.css

--- src/App/Main/Products/ProductsList.js
+++ src/App/Main/Products/ProductsList.js
@@ -3,12 +3,7 @@ import React from 'react'
 const ProductsList = () => {
     return (
             <div>
-               <h1 style={{
-                   color: "white",
-                   fontSize: "32px",
-                   marginTop: "10px",
-                    textAlign: "center",
-               }}>Products List</h1>
+                <h1 className="page-title">Products List</h1>
             </div>
     )
 }
