3a0fc3dabe9ab4467accd4aca669cda4face84c9
Yuliia <yuliia.brakova@gmail.com>
2020-02-25 19:16:33 +0200

    добавили стили для h1

---
+++ src/App/Main/Products/ProductsList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductsList = () => {
+    return (
+            <div>
+               <h1 style={{
+                   color: "white",
+                   fontSize: "32px",
+                   marginTop: "10px",
+                    textAlign: "center",
+               }}>Products List</h1>
+            </div>
+    )
+}
+export default ProductsList 
