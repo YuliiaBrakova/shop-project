a90a494aec6b68b5d07332bd34356e478277b3e7
Yuliia <yuliia.brakova@gmail.com>
2020-02-25 19:29:57 +0200

    добавили разметку для export default ProductListItem

---
+++ src/App/Main/Products/ProductListItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+
+const ProductListItem = () => {
+    return (
+        <div className="product-list-item">
+            <div className="product-title">Iphone X</div>
+            <div className="product-description">This is Iphone X</div>
+            <div className="product-features">Type: phone</div>
+            <div className="product-features">Capacite: 64 Gb</div>
+            <div className="product-price">500$</div>
+            <div className="add-to-cart-btn">Add to cart</div>
+        </div>
+    )
+}
+export default ProductListItem
