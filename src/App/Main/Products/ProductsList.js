1e5631b4a4b9b5dd038f8c6cb61b758256540c20
Yuliia <yuliia.brakova@gmail.com>
2020-02-26 23:30:11 +0200

    добавили key для React

--- src/App/Main/Products/ProductsList.js
+++ src/App/Main/Products/ProductsList.js
@@ -9,8 +9,8 @@ const ProductsList = () => {
                 <h1 className="page-title">Products List</h1>
                 <div className="row">
                    {
-                   productsData.map((product) =>(
-                      <div className="col-lg-6">
+                   productsData.map((product,index) =>(
+                      <div className="col-lg-6" key={index}>
                         <ProductListItem
                             name={product.name}
                             description={product.description}
